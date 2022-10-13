fcdcf30b01b6334f2943c433017bb28e130b8e25
elizarraga <eduardo.alejandro.lizarraga@gmail.com>
2022-10-12 21:36:27 -0300

    erro na collection de pets

--- src/routes/pet.js
+++ src/routes/pet.js
@@ -3,21 +3,21 @@ const petSchema = require("../models/pet");
 
 const router = express.Router();
 
-//get all pets
-router.get('/pets', (req,res) => {
+//get todas as pets
+router.get('/pets', (req, res) => {
     petSchema
     .find()
     .populate('tutor')
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(400).json({message: err}));
+    .then((data) => res.json({ Pets: data }))
+    .catch((err) => res.json({ message: err }));
 });
 
 // create pet
-router.post('/pets', async (req,res) => {
+router.post('/pet', async (req, res) => {
     const pet = petSchema(req.body);
     try{
         const newPet = await pet.save(pet)
-        res.status(200).json(newPet)
+        res.status(201).json({ Novo_Pet: newPet })
     }
    
     catch(err){
@@ -26,7 +26,7 @@ router.post('/pets', async (req,res) => {
             return;
         }
         if(err.code == 11000){
-            res.status(400).send('numero de pet já existe')
+            res.status(400).json({ message: 'numero de pet já existe' })
             return;
         }
         else{
@@ -37,91 +37,201 @@ router.post('/pets', async (req,res) => {
 });
 
 
-//get pet
-router.get('/pets/:id', (req,res) => {
+//get pet pela id
+router.get('/pet/:id', (req, res) => {
     const { id } = req.params;
     petSchema
     .findById(id)
     .populate('tutor')
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(400).json({message: err}));
+    .then((data) =>{
+        
+        if(data == null){
+            res.status(404).json({ message: 'id não encontrado, a id não existe', id})
+            return;
+        }
+        else{
+            res.status(200).json({ Pet: data })
+        }
+        
+
+    })
+    .catch((err) =>{
+    if(err.name == 'CastError'){
+        res.status(400).json({message: err});
+        return;
+    }
+ 
+    else {res.status(500).json({message: err});
+            return;
+        }
+})
+
+
 });
 
-//Get pet pela Cpf do tutor
-router.get('/pets/cpf/:cpf', (req,res) => {
-    const {cpf}  = req.params;
-    petSchema
-    .findOne({ cpf: cpf})
-    .populate('tutor')
-    .then((data) => res.json(data))
-    .catch((err) => res.json({message: err}));
-});
+
 
 //Get pet pelo numero do pet
-router.get('/pets/nroPet/:nroPet', (req,res) => {
+router.get('/pet/nroPet/:nroPet', (req, res) => {
     const {nroPet}  = req.params;
     petSchema
-    .findOne({ nroPet: nroPet})
+    .findOne({ nroPet: nroPet })
     .populate('tutor')
-    .then((data) => res.json(data))
-    .catch((err) => res.json({message: err}));
+    .then((data) =>{
+        
+        if(data == null){
+            res.status(404).json({ message: 'numero de pet não encontrado, numero de pet não cadastrado', nroPet})
+            return;
+        }
+        else{
+            res.status(200).json({ Pet: data })
+        }
+        
+
+    })
+    .catch((err) =>{
+    if(err.name == 'CastError'){
+        res.status(400).json({message: err});
+        return;
+    }
+ 
+    else {res.status(500).json({message: err});
+            return;
+        }
+})
+
+
 });
 
 //update pet pela id
-router.put('/pets/:id', (req,res) => {
+router.put('/pet/:id', async (req, res) => {
     const { id } = req.params;
+    const pet = await petSchema.findOne({ _id: id });
     const { nroPet, nome, idade, peso, raça, tipo, tutor } = req.body;
-    petSchema
-    .updateOne({ _id: id }, { $set: { nroPet, nome, idade, peso, raça, tipo, tutor }})
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(400).json({message: err}));
+    
+    try{
+
+        if(!pet){
+        res.status(404).json({ message: 'id não encontrado, id não existe', id })
+        return;
+      }
+        else{
+        pet.set(req.body);
+         await  pet.save();
+        res.status(200).json({ message: 'Dados de pet atualizado', pet })
+        }
+        
+    }
+    catch(err){
+      
+        if(err.name == "ValidationError"){
+            res.status(400).json({message: err})
+            return;
+        }
+        if(err.code == 11000){
+            res.status(400).json({ message: 'Numero de pet já existe'})
+            return;
+        }
+        else{
+            res.status(500).json({message: err});
+        }
+
+    } 
+
 });
 
-//update pet pela cpf do tutor
-router.put('/pets/cpf/:cpf', (req,res) => {
-    const { cpf } = req.params;
-    const { nroPet, nome, idade, peso, raça, tipo, tutor } = req.body;
-    petSchema
-    .updateOne({ cpf: cpf }, { $set: { nroPet, nome, idade, peso, raça, tipo, tutor }})
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(400).json({message: err}));
-});
 
 //update pet pelo numero de pet
-router.put('/pets/nroPet/:nroPet', (req,res) => {
+router.put('/pet/nroPet/:nroPet', async (req, res) => {
     const { nroPet } = req.params;
+    const pet = await petSchema.findOne({ nroPet: nroPet });
     const {nome, idade, peso, raça, tipo, tutor } = req.body;
-    petSchema
-    .updateOne({ nroPet: nroPet }, { $set: {nome, idade, peso, raça, tipo, tutor }})
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(400).json({message: err}));
+
+    try{
+   
+    if(!pet){
+        res.status(404).json({ message: 'numero de pet não encontrado, numero de pet não cadastrado', nroPet})
+        return;
+    } 
+    else{
+        pet.set(req.body);
+         await  pet.save();
+        res.status(200).json({ message: 'dados de pet atualizado', pet })
+    }
+        
+    }
+    
+    
+    catch(err){
+        if(err.name == "ValidationError"){
+            res.status(400).json({message: err})
+            return;
+        }
+      
+        else{
+            res.status(500).json({message: err});
+        }
+
+    }
 });
 
-//delete pet
-router.delete('/pets/:id', (req,res) => {
+//delete pet pela id
+router.delete('/pet/:id', (req, res) => {
     const { id } = req.params;
     petSchema
-    .remove({ _id: id })
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(400).json({message: err}));
+    .findOneAndDelete({ _id: id })
+    .then((data) =>{
+        if(data == null){
+            res.status(404).json({ message: 'id não encontrado, id não existe', id})
+            return;
+        }
+        else{
+            res.status(200).json({ message: 'a pet foi excluido com sucesso!' })
+        }
+        
+
+    })
+    .catch((err) =>{
+    if(err.name == 'CastError'){
+        res.status(400).json({message: err});
+        return;
+    }
+ 
+    else {res.status(500).json({message: err});
+            return;
+        }
+})
 });
 
-//delete pelo cpf
-router.delete('/pets/cpf/:cpf', (req,res) => {
-    const {cpf}  = req.params;
-    petSchema
-    .remove({ cpf: cpf})
-    .then((data) => res.json(data))
-    .catch((err) => res.json({message: err}));
-});
 
 //delete pelo numero de pet
-router.delete('/pets/nroPet/:nroPet', (req,res) => {
-    const {nroPet}  = req.params;
+router.delete('/pet/nroPet/:nroPet', (req, res) => {
+    const {nroPet} = req.params;
     petSchema
-    .remove({ nroPet: nroPet})
-    .then((data) => res.json(data))
-    .catch((err) => res.json({message: err}));
+    .findOneAndDelete({ nroPet: nroPet })
+    .then((data) =>{
+        
+        if(data == null){
+            res.status(404).json({ message: 'numero de pet não encontrado, numero de pet não existe', nroPet })
+            return;
+        }
+    
+        else{
+            res.status(200).json({ message: 'a pet foi excluido com sucesso!' })
+        }
+        
+
+    })
+    .catch((err) =>{
+    if(err.name == 'CastError'){
+        res.status(400).json({message: err});
+        return;
+    }
+ 
+    else {res.status(500).json({message: err});
+            return;
+        }
+})
 });
 
 
